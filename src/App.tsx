46168366f276cbdbb7b6e0a88cae56a80ab981af
Cesar Escobar <cesarsilvaescobar@gmail.com>
2023-09-12 18:23:29 -0300

    Fim da primeira aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,133 @@
-import { FileVideo, Github } from "lucide-react"
-import { Button } from "./components/ui/button"
-import { Separator } from "./components/ui/separator"
-import { Textarea } from "./components/ui/textarea"
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react"
 import { Label } from "./components/ui/label"
+
+import { Textarea } from "./components/ui/textarea"
+import { Separator } from "./components/ui/separator"
+import { Button } from "./components/ui/button"
+import {
+  Select,
+  SelectItem,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+} from "./components/ui/select"
+import { Slider } from "./components/ui/slider"
 
 export function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
+        <h1 className="text-xl font-bold">Upload.ai</h1>
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com ❤ no NLW da RocketSeat
+            Desenvolvido com ❤️ no Nlw da RocketSeat
           </span>
           <Separator orientation="vertical" className="h-6" />
-          <Button variant="outline">
+          <Button variant={"outline"}>
             <Github className="w-4 h-4 mr-2" />
             Github
           </Button>
         </div>
       </div>
+
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Inclua um prompt para a IA..."
+              placeholder="Inclua o prompt para a IA..."
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Resultado gerado pela IA.."
+              placeholder="Resultado gerado pela IA"
               readOnly
             />
           </div>
           <p className="text-sm text-muted-foreground">
             Lembre-se: você pode utilizar a variável{" "}
-            <code className="text-violet-400">{"{transcription}"}</code> no seu
-            prompt para adicionar o conteúdo da transcrição do vídeo
-            selecionado.
+            <code className="text-red-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo selecionado
           </p>
         </div>
         <aside className="w-80 space-y-6">
           <form className="space-y-6">
             <label
               htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed 
-            text-sm flex-col gap-2 items-center justify-center text-muted-foreground
-            hover:bg-primary/5"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
             >
               <FileVideo className="w-4 h-4" />
-              Selecione um Vídeo
+              Selecione um video
             </label>
             <input
               type="file"
-              name=""
+              name="video"
               id="video"
               accept="video/mp4"
               className="sr-only"
             />
+
             <Separator />
-            <div className="space-y-6">
+
+            <div className="space-y-2">
               <Label htmlFor="transcription_prompt">
                 Prompt de transcrição
               </Label>
               <Textarea
                 id="transcription_prompt"
-                className="min-h-20 leading-relaxed"
-                placeholder="Inclua palavras-chaves mencionadas no vídeo, separadas por vírgula(,)"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras chaves mencionadas no video separadas por vírgula ( , )"
               />
             </div>
+            <Button type="submit" className="w-full">
+              Carregar Video
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+          <Separator />
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um Prompt" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Descrição do Youtube</SelectItem>
+                  <SelectItem value="description">Título do YouTube</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-Turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros
+              </span>
+            </div>
+            <Separator />
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
           </form>
         </aside>
       </main>
